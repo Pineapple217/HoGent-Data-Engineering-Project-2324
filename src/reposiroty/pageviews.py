3faf39111296521dd55b2370d557bec5d125f45b
JulesRosier <julesrosier@student.hogent.be>
2023-10-05 20:38:24 +0200

    4x Speed door threading

--- src/reposiroty/pageviews.py
+++ src/reposiroty/pageviews.py
@@ -9,6 +9,10 @@ from repository import get_engine
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
+import concurrent.futures
+
+NUM_THREADS = 2
+BATCH_SIZE = 10_000
 
 logger = logging.getLogger(__name__)
 
@@ -38,6 +42,10 @@ class Pageview(Base):
     Status: Mapped[str] = mapped_column(String(50), nullable=True)
     Reden_van_status: Mapped[str] = mapped_column(String(50), nullable=True)
 
+def insert_pageviews_data(pageviews_data, session):
+    session.bulk_save_objects(pageviews_data)
+    session.commit()
+
 def seed_pageviews():
     engine = get_engine()
     Session = sessionmaker(bind=engine)
@@ -52,42 +60,43 @@ def seed_pageviews():
     df['crm CDI_PageView[Aangemaakt op]'] = pd.to_datetime(df['crm CDI_PageView[Aangemaakt op]'], format='%d/%m/%Y')
     df['crm CDI_PageView[Gewijzigd op]'] = pd.to_datetime(df['crm CDI_PageView[Gewijzigd op]'], format='%d/%m/%Y')
     pageviews_data = []
-    BATCH_SIZE = 10_000
     logger.info("Seeding inserting rows")
     progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
-    for i, row in df.iterrows():
-        p = Pageview(
-            Anonymous_Visitor=row['crm CDI_PageView[Anonymous Visitor]'],
-            Browser=row['crm CDI_PageView[Browser]'],
-            Campaign=row['crm CDI_PageView[Campaign]'],
-            Contact=row['crm CDI_PageView[Contact]'],
-            Duration=row['crm CDI_PageView[Duration]'],
-            Operating_System=row['crm CDI_PageView[Operating System]'],
-            Page_View=row['crm CDI_PageView[Page View]'],
-            Referrer_Type=row['crm CDI_PageView[Referrer Type]'],
-            Time=row['crm CDI_PageView[Time]'],
-            Page_Title=row['crm CDI_PageView[Page Title]'],
-            Type=row['crm CDI_PageView[Type]'],
-            Url=row['crm CDI_PageView[Url]'],
-            Viewed_On=row['crm CDI_PageView[Viewed On]'],
-            Visit=row['crm CDI_PageView[Visit]'],
-            Visitor_Key=row['crm CDI_PageView[Visitor Key]'],
-            Web_Content=row['crm CDI_PageView[Web Content]'],
-            Aangemaakt_op=row['crm CDI_PageView[Aangemaakt op]'],
-            Gewijzigd_door=row['crm CDI_PageView[Gewijzigd door]'],
-            Gewijzigd_op=row['crm CDI_PageView[Gewijzigd op]'],
-            Status=row['crm CDI_PageView[Status]'],
-            Reden_van_status=row['crm CDI_PageView[Reden van status]']
-        )
-        pageviews_data.append(p)
-        
-        if len(pageviews_data) >= BATCH_SIZE:
-            session.bulk_save_objects(pageviews_data)
-            session.commit()
-            pageviews_data = []
-            progress_bar.update(BATCH_SIZE)
+    with concurrent.futures.ThreadPoolExecutor(max_workers=NUM_THREADS) as executor:
+        futures = []
+        for i, row in df.iterrows():
+            p = Pageview(
+                Anonymous_Visitor=row['crm CDI_PageView[Anonymous Visitor]'],
+                Browser=row['crm CDI_PageView[Browser]'],
+                Campaign=row['crm CDI_PageView[Campaign]'],
+                Contact=row['crm CDI_PageView[Contact]'],
+                Duration=row['crm CDI_PageView[Duration]'],
+                Operating_System=row['crm CDI_PageView[Operating System]'],
+                Page_View=row['crm CDI_PageView[Page View]'],
+                Referrer_Type=row['crm CDI_PageView[Referrer Type]'],
+                Time=row['crm CDI_PageView[Time]'],
+                Page_Title=row['crm CDI_PageView[Page Title]'],
+                Type=row['crm CDI_PageView[Type]'],
+                Url=row['crm CDI_PageView[Url]'],
+                Viewed_On=row['crm CDI_PageView[Viewed On]'],
+                Visit=row['crm CDI_PageView[Visit]'],
+                Visitor_Key=row['crm CDI_PageView[Visitor Key]'],
+                Web_Content=row['crm CDI_PageView[Web Content]'],
+                Aangemaakt_op=row['crm CDI_PageView[Aangemaakt op]'],
+                Gewijzigd_door=row['crm CDI_PageView[Gewijzigd door]'],
+                Gewijzigd_op=row['crm CDI_PageView[Gewijzigd op]'],
+                Status=row['crm CDI_PageView[Status]'],
+                Reden_van_status=row['crm CDI_PageView[Reden van status]']
+            )
+            pageviews_data.append(p)
+            
+            if len(pageviews_data) >= BATCH_SIZE:
+                futures.append(executor.submit(insert_pageviews_data, pageviews_data, session))
+                pageviews_data = []
+                progress_bar.update(BATCH_SIZE)
 
-    # Insert any remaining data
-    if pageviews_data:
-        session.bulk_save_objects(pageviews_data)
-        session.commit()
+        # Insert any remaining data
+        if pageviews_data:
+            futures.append(executor.submit(insert_pageviews_data, pageviews_data, session))
+
+        concurrent.futures.wait(futures)
