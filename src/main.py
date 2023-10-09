dd7008425c529b0edec7c70805320f6fd8c47d2f
senne dierick <sennedierick@hotmail.be>
2023-10-09 22:51:26 +0200

    added option to test if database is filled

--- src/main.py
+++ src/main.py
@@ -1,6 +1,7 @@
 import argparse
 import logging
 from repository.main import db_init, db_seed
+from test.main import db_test
 
 logging.basicConfig(
     level=logging.INFO,
@@ -11,12 +12,19 @@ logging.basicConfig(
 )
 logger = logging.getLogger(__name__)
 
+
 def main():
     parser = argparse.ArgumentParser(description="DEP Cli die heel cool is")
-    subparsers = parser.add_subparsers(dest='Command')
-    subparsers.add_parser("db_init", help="Create database and tables").set_defaults(func=db_init)
-    subparsers.add_parser("db_seed", help="Fills all tables of the database").set_defaults(func=db_seed)
-
+    subparsers = parser.add_subparsers(dest="Command")
+    subparsers.add_parser("db_init", help="Create database and tables").set_defaults(
+        func=db_init
+    )
+    subparsers.add_parser(
+        "db_seed", help="Fills all tables of the database"
+    ).set_defaults(func=db_seed)
+    subparsers.add_parser(
+        "db_test", help="Drops all tables and recreates them"
+    ).set_defaults(func=db_test)
     args = parser.parse_args()
 
     if not hasattr(args, "func"):
---
+++ src/test/main.py
@@ -0,0 +1,48 @@
+import logging
+import os
+from dotenv import load_dotenv
+from sqlalchemy import create_engine, MetaData, Table
+from sqlalchemy.orm import declarative_base, sessionmaker
+from repository.base import Base
+
+logger = logging.getLogger(__name__)
+
+load_dotenv()
+DB_URL = os.getenv("DB_URL")
+DATA_PATH = os.getenv("DATA_PATH")
+engine = create_engine(DB_URL, connect_args={"connect_timeout": 100})
+conn = engine.connect()
+metadata = MetaData()
+
+Base = declarative_base()
+Base.metadata.reflect(engine)
+table_names = [
+    "Pageviews",
+    "SessieInschrijving",
+    "Sessie",
+    "Inschrijving",
+    "Gebruiker",
+    "InfoEnKlachten",
+    "Campagne",
+]
+
+table_names = Base.metadata.tables.keys()
+Session = sessionmaker(bind=engine)
+session = Session()
+
+
+def db_test():
+    logger.info("Testing database...")
+
+    for table_name in table_names:
+        emp_table = Table(table_name, Base.metadata, autoload_with=engine)
+        query = emp_table.select().limit(1)
+        result = session.execute(query)
+        print("")
+        logger.info(" 1 row from table: " + table_name)
+        print(result.fetchall())
+        session.commit()
+
+    logger.info("Database test succesfull")
+
+    session.close()
