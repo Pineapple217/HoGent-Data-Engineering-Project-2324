031f212e2d55a1b2549a7c714322e8459f6b6bfb
Hannelore Mergan <hannelore.mergan@student.hogent.be>
2023-11-08 10:04:37 +0100

    materialized view id's weg

--- src/repository/materialized_view.sql
+++ src/repository/materialized_view.sql
@@ -2,26 +2,23 @@
 
 -- CREATE MATERIALIZED VIEW IF NOT EXISTS materialized_view_ml AS
 SELECT 
-    Campagne.CampagneId,
-    Campagne.RedenVanStatus,
+    Campagne.RedenVanStatus as campagne_statusreden,
     Campagne.Startdatum,
     Campagne.TypeCampagne,
     Campagne.SoortCampagne,
 
-    Inschrijving.InschrijvingId,
+    Persoon.Marketingcommunicatie,
+    Persoon.RedenVanStatus as persoon_statusreden,
+
     Inschrijving.AanwezigAfwezig,
     Inschrijving.Bron,
-    Inschrijving.DatumInschrijving,
-
-    SessieInschrijving.SessieInschrijvingId,
-
-    Sessie.SessieId,
+    
     Sessie.Activiteitstype,
-    Sessie.EindDatumTijd,
     Sessie.Product,
-    Sessie.StartDatumTijd,
     Sessie.ThemaNaam
 FROM Campagne
 JOIN Inschrijving ON Inschrijving.CampagneId = Campagne.CampagneId
 JOIN SessieInschrijving ON SessieInschrijving.InschrijvingId = Inschrijving.InschrijvingId
 JOIN Sessie ON Sessie.SessieId = SessieInschrijving.SessieId
+JOIN Contactfiche on Contactfiche.ContactpersoonId = Inschrijving.ContactficheId
+JOIN Persoon ON Persoon.PersoonId = Contactfiche.PersoonId
