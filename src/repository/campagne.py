25a2df2cbb3686cc63920a42b569ae3a6b04f9ba
Hannelore Mergan <hannelore.mergan@student.hogent.be>
2023-11-16 19:49:46 +0100

    epic 2: add new data voor campagne only

--- src/repository/campagne.py
+++ src/repository/campagne.py
@@ -5,6 +5,8 @@ from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, relationship
 from sqlalchemy import String
 from sqlalchemy.dialects.mssql import DATETIME2
 from repository.main import get_engine, DATA_PATH
+import os
+import datetime
 import pandas as pd
 import numpy as np
 from typing import TYPE_CHECKING
@@ -46,45 +48,76 @@ def insert_campagne_data(campagne_data, session):
     session.bulk_save_objects(campagne_data)
     session.commit()
 
+def move_csv_file(csv_path, destination_folder, timestamp=True):
+    if timestamp:
+        timestamp_str = datetime.datetime.now().strftime("%Y_W%U")
+        base_name = os.path.basename(csv_path)
+        new_path = os.path.join(destination_folder, f"{base_name}_{timestamp_str}")
+    else:
+        new_path = os.path.join(destination_folder, os.path.basename(csv_path))
+    
+    os.rename(csv_path, new_path)
 
 def seed_campagne():
     engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    logger.info("Reading CSV...")
-    csv = DATA_PATH + "/Campagnes.csv"
-    df = pd.read_csv(csv, delimiter=",", encoding="utf-8", keep_default_na=True, na_values=[""])
-    
-    df = df.replace({np.nan: None})
 
-    df["crm_Campagne_Einddatum"] = pd.to_datetime(df["crm_Campagne_Einddatum"], format=DATE_FORMAT)
-    df["crm_Campagne_Startdatum"] = pd.to_datetime(df["crm_Campagne_Startdatum"], format=DATE_FORMAT)
+    existing_ids = set()
 
-    campagne_data = []
-    logger.info("Seeding inserting rows")
-    progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
-    for _, row in df.iterrows():
-        p = Campagne(
-            CampagneId=row["crm_Campagne_Campagne"],
-            CampagneNr=row["crm_Campagne_Campagne_Nr"],
-            Einddatum=row["crm_Campagne_Einddatum"],
-            CampagneNaam=row["crm_Campagne_Naam"],
-            NaamInMail=row["crm_Campagne_Naam_in_email"],
-            RedenVanStatus=row["crm_Campagne_Reden_van_status"],
-            Startdatum=row["crm_Campagne_Startdatum"],
-            Status=row["crm_Campagne_Status"],
-            TypeCampagne=row["crm_Campagne_Type_campagne"],
-            URLVoka=row["crm_Campagne_URL_voka_be"],
-            SoortCampagne=row["crm_Campagne_Soort_Campagne"],
-        )
-        campagne_data.append(p)
+    old_csv_dir = os.path.join(DATA_PATH, "old")
+    new_csv_dir = os.path.join(DATA_PATH, "new")
 
-        if len(campagne_data) >= BATCH_SIZE:
-            insert_campagne_data(campagne_data, session)
-            campagne_data = []
-            progress_bar.update(BATCH_SIZE)
+    for folder in [old_csv_dir, new_csv_dir]:  # Process both "old" and "new" folders
+        logger.info(f"Processing CSV files in '{folder}' folder...")
 
-    if campagne_data:
-        insert_campagne_data(campagne_data, session)
-        progress_bar.update(len(campagne_data))
+        for filename in os.listdir(folder):
+            if filename.startswith("Campagne"):
+                csv_path = os.path.join(folder, filename)
+
+                logger.info(f"Reading CSV: {csv_path}")
+                df = pd.read_csv(csv_path, delimiter=",", encoding="utf-8", keep_default_na=True, na_values=[""])
+
+                df = df.replace({np.nan: None})
+
+                df["crm_Campagne_Einddatum"] = pd.to_datetime(df["crm_Campagne_Einddatum"], format=DATE_FORMAT)
+                df["crm_Campagne_Startdatum"] = pd.to_datetime(df["crm_Campagne_Startdatum"], format=DATE_FORMAT)
+
+                df_no_duplicates = df[~df["crm_Campagne_Campagne"].isin(existing_ids)]
+                existing_ids.update(df_no_duplicates["crm_Campagne_Campagne"])
+
+                new_rows_count = len(df_no_duplicates)
+
+                campagne_data = []
+                logger.info("Seeding inserting rows")
+                progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+                for _, row in df_no_duplicates.iterrows():
+                    p = Campagne(
+                        CampagneId=row["crm_Campagne_Campagne"],
+                        CampagneNr=row["crm_Campagne_Campagne_Nr"],
+                        Einddatum=row["crm_Campagne_Einddatum"],
+                        CampagneNaam=row["crm_Campagne_Naam"],
+                        NaamInMail=row["crm_Campagne_Naam_in_email"],
+                        RedenVanStatus=row["crm_Campagne_Reden_van_status"],
+                        Startdatum=row["crm_Campagne_Startdatum"],
+                        Status=row["crm_Campagne_Status"],
+                        TypeCampagne=row["crm_Campagne_Type_campagne"],
+                        URLVoka=row["crm_Campagne_URL_voka_be"],
+                        SoortCampagne=row["crm_Campagne_Soort_Campagne"],
+                    )
+                    campagne_data.append(p)
+
+                    if len(campagne_data) >= BATCH_SIZE:
+                        insert_campagne_data(campagne_data, session)
+                        campagne_data = []
+                        progress_bar.update(BATCH_SIZE)
+
+                if campagne_data:
+                    insert_campagne_data(campagne_data, session)
+                    progress_bar.update(len(campagne_data))
+
+                move_csv_file(csv_path, old_csv_dir, timestamp=(folder == new_csv_dir))
+
+                print(f"Number of new (non-duplicate) rows found in {csv_path}: {new_rows_count}")
+
+    session.close()
