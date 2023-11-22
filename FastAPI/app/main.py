b5c0bf6aaadb40a6f769f1b631b95a30d7997b91
Senne Dierick <senne.dierick@student.hogent.be>
2023-11-22 20:10:44 +0100

    test

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -19,11 +19,30 @@ query = """
     join Functie f on f.FunctieId = cf.FunctieId
     where CampagneId is not null;
 """
-df = pd.read_sql(query, engine)
-df.set_index('ContactPersoonId', inplace=True)
-df["rating"] = 1
-df_pivot = pd.pivot_table(df, index='ContactPersoonId', columns=['Ondernemingsaard', 'Ondernemingstype', 'PrimaireActiviteit', 'Functie'], values='rating', fill_value = 0)
+df1 = pd.read_sql(query, engine)
+df1.set_index('ContactPersoonId', inplace=True)
+df1["rating"] = 5
 
+query2 = """
+    with pageview_count as(
+        select p.PageTitle, count(p.ContactId) as count
+        from Pageviews p
+        group by p.PageTitle
+    )
+    select PageTitle, ContactId
+    from Pageviews
+    where Pagetitle in (select top (2500) PageTitle
+    from pageview_count
+    order by count desc)
+"""
+df2 = pd.read_sql(query2, engine)
+df2.set_index('ContactId', inplace=True)
+df2["rating"] = 1
+df1_pivot = pd.pivot_table(df1, index='ContactPersoonId', columns=['Ondernemingsaard', 'Ondernemingstype', 'PrimaireActiviteit', 'Functie'], values='rating', fill_value = 0).astype('float16')
+df2_pivot = pd.pivot_table(df2, index='ContactId', columns=['PageTitle'], values='rating', fill_value = 0).astype('float16')
+
+df_pivot = pd.concat([df1_pivot, df2_pivot], axis=1, join='inner').astype('float16')
+del [[ df1_pivot, df2_pivot]]
 
 def calc(contact_id: str):
     select_contact = contact_id
@@ -31,12 +50,11 @@ def calc(contact_id: str):
     similarities = df_pivot.index.join(similarity_matrix)
     similarities = pd.DataFrame({'ContactPersoonId':df_pivot.index, 'sim':similarity_matrix}).set_index('ContactPersoonId')
     similar_users = similarities[similarities['sim'] > 0.75].sort_values(by='sim', ascending=False)
-    done_campaigns = df.loc[df.index == select_contact]['CampagneId']
-    similar_campagnes = df[df.index.isin(similar_users.index)].replace(0, np.nan).dropna(axis=1, how='all')
-    similar_campagnes_not_done = similar_campagnes[~similar_campagnes['CampagneId'].isin(done_campaigns)][['CampagneId', 'CampagneNaam']]
-    similar_campagnes_not_done.set_index('CampagneId', inplace=True)
-    similar_campagnes_not_done.drop_duplicates(inplace=True)
-    return similar_campagnes_not_done
+    done_campaigns = df1.loc[df.index == select_contact]['CampagneId']
+    similar_campagnes = df1[df1.index.isin(similar_users.index)].replace(0, np.nan).dropna(axis=1, how='all')
+    similar_campagnes = similar_campagnes.drop_duplicates(subset=['CampagneId'])
+    similar_campagnes = similar_campagnes[similar_campagnes['Startdatum'] > '2023-11-16']
+    return similar_campagnes
 
 
 app = FastAPI()
