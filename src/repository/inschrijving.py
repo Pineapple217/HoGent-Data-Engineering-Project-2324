c9af39f2ea0008692e0707c576ba9c5e7bc2a022
WardDeDeckere <ward.dedeckere@student.hogent.be>
2023-11-23 17:57:25 +0100

    Inschrijving Epic 2 update

--- src/repository/inschrijving.py
+++ src/repository/inschrijving.py
@@ -1,10 +1,11 @@
 from .base import Base
-
+from .functionalities import load_csv, move_csv_file
 import logging
 from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, relationship
 from sqlalchemy import String, Float, ForeignKey, text
 from sqlalchemy.dialects.mssql import DATETIME2
 from repository.main import get_engine, DATA_PATH
+import os
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -51,44 +52,70 @@ def to_float(x):
     except:
         return float(x.replace(",", ".").replace("'", ""))
 
+def get_existing_ids(session):
+    return [result[0] for result in session.query(Inschrijving.InschrijvingId).all()]
 
 def seed_inschrijving():
     engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
+
+    existing_ids = get_existing_ids(session)
+
+    old_csv_dir = os.path.join(DATA_PATH, "old")
+    new_csv_dir = os.path.join(DATA_PATH, "new")
+    if not os.path.exists(old_csv_dir) or not os.path.exists(new_csv_dir):
+        raise FileNotFoundError("The folders 'old' and 'new' must exist in the data folder")
     
-    logger.info("Reading CSV...")
-    csv = DATA_PATH + "/Inschrijvingen.csv"
-    df = pd.read_csv(csv, delimiter=",", encoding="utf-8", keep_default_na=True, na_values=[""])
-    
-    df = df.replace({np.nan: None})
- 
-    df["crm_Inschrijving_Datum_inschrijving"] = pd.to_datetime(df["crm_Inschrijving_Datum_inschrijving"], format="%d-%m-%Y")
-    
+    folder_new = new_csv_dir
+
     inschrijving_data = []
-    logger.info("Seeding inserting rows")
-    progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
-    for _, row in df.iterrows():
-        p = Inschrijving(
-            InschrijvingId=row["crm_Inschrijving_Inschrijving"],
-            AanwezigAfwezig=row["crm_Inschrijving_Aanwezig_Afwezig"],
-            Bron=row["crm_Inschrijving_Bron"],
-            ContactficheId=row["crm_Inschrijving_Contactfiche"],
-            DatumInschrijving=row["crm_Inschrijving_Datum_inschrijving"],
-            FacturatieBedrag=to_float(row["crm_Inschrijving_Facturatie_Bedrag"]),
-            CampagneId=row["crm_Inschrijving_Campagne"],
-            CampagneNaam=row["crm_Inschrijving_Campagne_Naam_"],
-        )
-        inschrijving_data.append(p)
+    for filename in os.listdir(folder_new): #check alle filenames in 'new'
+        if filename == 'Inschrijving.csv' or filename == "Inschrijvingen.csv": #kan ook met startswith
+            csv_path = os.path.join(folder_new, filename)
+    
+            logger.info(f"Reading CSV: {csv_path}")
+            df, error = load_csv(csv_path)
+            if error:
+                raise Exception(f"Error loading CSV: {csv_path, error}")
+            
+            df = df.replace({np.nan: None})
 
-        if len(inschrijving_data) >= BATCH_SIZE:
-            insert_inschrijving_data(inschrijving_data, session)
-            inschrijving_data = []
-            progress_bar.update(BATCH_SIZE)
+            df = df[~df['crm_Inschrijving_Inschrijving'].isin(existing_ids)]  
 
-    if inschrijving_data:
-        insert_inschrijving_data(inschrijving_data, session)
-        progress_bar.update(len(inschrijving_data))
+            df["crm_Inschrijving_Datum_inschrijving"] = pd.to_datetime(df["crm_Inschrijving_Datum_inschrijving"], format="%d-%m-%Y")
+
+            # data in chunks steken
+            chunks = [df[i:i + BATCH_SIZE] for i in range(0, df.shape[0], BATCH_SIZE)]
+
+            progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+
+            for chunk in chunks:
+                inschrijving_data = []
+                for _, row in chunk.iterrows():
+                    p = Inschrijving(
+                        InschrijvingId=row["crm_Inschrijving_Inschrijving"],
+                        AanwezigAfwezig=row["crm_Inschrijving_Aanwezig_Afwezig"],
+                        Bron=row["crm_Inschrijving_Bron"],
+                        ContactficheId=row["crm_Inschrijving_Contactfiche"],
+                        DatumInschrijving=row["crm_Inschrijving_Datum_inschrijving"],
+                        FacturatieBedrag=to_float(row["crm_Inschrijving_Facturatie_Bedrag"]),
+                        CampagneId=row["crm_Inschrijving_Campagne"],
+                        CampagneNaam=row["crm_Inschrijving_Campagne_Naam_"],
+                    )
+                    inschrijving_data.append(p)
+
+                inschrijving_data(inschrijving_data, session)
+                progress_bar.update(len(inschrijving_data))
+
+            progress_bar.close()
+
+            move_csv_file(csv_path, old_csv_dir)
+
+            logger.info(f"Number of new (non-duplicate) rows found in {csv_path}: {len(df)}")
+
+    if not inschrijving_data:
+        logger.info("No new data was given. Data is up to date already.")
 
     session.execute(
         text(
