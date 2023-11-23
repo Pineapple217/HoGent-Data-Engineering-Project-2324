65c78e3621a8af987ecbe36bc1f16c8f6a299d20
WardDeDeckere <ward.dedeckere@student.hogent.be>
2023-11-23 18:46:19 +0100

    Sessie Epic 2 update

--- src/repository/sessie.py
+++ src/repository/sessie.py
@@ -1,5 +1,5 @@
 from .base import Base
-
+from .functionalities import load_csv, move_csv_file
 import logging
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
@@ -7,6 +7,7 @@ from sqlalchemy import String, ForeignKey, text
 from sqlalchemy.dialects.mssql import DATETIME2
 from sqlalchemy.orm import sessionmaker
 from repository.main import get_engine, DATA_PATH
+import os
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -44,48 +45,71 @@ def insert_sessie_data(sessie_data, session):
     session.bulk_save_objects(sessie_data)
     session.commit()
 
+def get_existing_ids(session):
+    return [result[0] for result in session.query(Sessie.SessieId).all()]
 
 def seed_sessie():
     engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
-    
-    logger.info("Reading CSV...")
-    csv = DATA_PATH + "/Sessie.csv"
-    df = pd.read_csv(csv, delimiter=",", encoding="utf-8", keep_default_na=True, na_values=[""],)
-    
-    # Sommige lege waardes worden als NaN ingelezen
-    # NaN mag niet in een varchar
-    df = df.replace({np.nan: None})
 
-    df["crm_Sessie_Eind_Datum_Tijd"] = pd.to_datetime(df["crm_Sessie_Eind_Datum_Tijd"], format=DATE_FORMAT)
-    df["crm_Sessie_Start_Datum_Tijd"] = pd.to_datetime(df["crm_Sessie_Start_Datum_Tijd"], format=DATE_FORMAT)
+    existing_ids = get_existing_ids(session)
+
+    old_csv_dir = os.path.join(DATA_PATH, "old")
+    new_csv_dir = os.path.join(DATA_PATH, "new")
+    if not os.path.exists(old_csv_dir) or not os.path.exists(new_csv_dir):
+        raise FileNotFoundError("The folders 'old' and 'new' must exist in the data folder")
     
+    folder_new = new_csv_dir
+
     sessie_data = []
-    logger.info("Seeding inserting rows")
-    progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
-    for _, row in df.iterrows():
-        p = Sessie(
-            SessieId=row["crm_Sessie_Sessie"],
-            Activiteitstype=row["crm_Sessie_Activiteitstype"],
-            CampagneId=row["crm_Sessie_Campagne"],
-            EindDatumTijd=row["crm_Sessie_Eind_Datum_Tijd"],
-            Product=row["crm_Sessie_Product"],
-            SessieNr=row["crm_Sessie_Sessie_nr_"],
-            StartDatumTijd=row["crm_Sessie_Start_Datum_Tijd"],
-            ThemaNaam=row["crm_Sessie_Thema_Naam_"],
-        )
-        sessie_data.append(p)
+    for filename in os.listdir(folder_new): #check alle filenames in 'new'
+        if filename == 'Sessie.csv':
+            csv_path = os.path.join(folder_new, filename)
+    
+            logger.info(f"Reading CSV: {csv_path}")
+            df, error = load_csv(csv_path)
+            if error:
+                raise Exception(f"Error loading CSV: {csv_path, error}")
+            
+            df = df.replace({np.nan: None})
 
-        if len(sessie_data) >= BATCH_SIZE:
-            insert_sessie_data(sessie_data, session)
-            sessie_data = []
-            progress_bar.update(BATCH_SIZE)
+            df = df[~df['crm_Sessie_Sessie'].isin(existing_ids)]  
 
-    # Insert any remaining data
-    if sessie_data:
-        insert_sessie_data(sessie_data, session)
-        progress_bar.update(len(sessie_data))
+            df["crm_Sessie_Eind_Datum_Tijd"] = pd.to_datetime(df["crm_Sessie_Eind_Datum_Tijd"], format=DATE_FORMAT)
+            df["crm_Sessie_Start_Datum_Tijd"] = pd.to_datetime(df["crm_Sessie_Start_Datum_Tijd"], format=DATE_FORMAT)
+
+            # data in chunks steken
+            chunks = [df[i:i + BATCH_SIZE] for i in range(0, df.shape[0], BATCH_SIZE)]
+
+            progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+
+            for chunk in chunks:
+                sessie_data = []
+                for _, row in chunk.iterrows():
+                    p = Sessie(
+                        SessieId=row["crm_Sessie_Sessie"],
+                        Activiteitstype=row["crm_Sessie_Activiteitstype"],
+                        CampagneId=row["crm_Sessie_Campagne"],
+                        EindDatumTijd=row["crm_Sessie_Eind_Datum_Tijd"],
+                        Product=row["crm_Sessie_Product"],
+                        SessieNr=row["crm_Sessie_Sessie_nr_"],
+                        StartDatumTijd=row["crm_Sessie_Start_Datum_Tijd"],
+                        ThemaNaam=row["crm_Sessie_Thema_Naam_"],
+                    )
+                    sessie_data.append(p)
+
+                insert_sessie_data(sessie_data, session)
+                progress_bar.update(len(sessie_data))
+
+            progress_bar.close()
+
+            move_csv_file(csv_path, old_csv_dir)
+
+            logger.info(f"Number of new (non-duplicate) rows found in {csv_path}: {len(df)}")
+
+    if not sessie_data:
+        logger.info("No new data was given. Data is up to date already.")
 
     session.execute(
         text(
