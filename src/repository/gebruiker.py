69d022497f6e150d5f31f7247ab941ab592e01bf
Hannelore Mergan <hannelore.mergan@student.hogent.be>
2023-11-23 15:06:26 +0100

    Epic 2 gebruiker

--- src/repository/gebruiker.py
+++ src/repository/gebruiker.py
@@ -1,9 +1,12 @@
 from .base import Base
+from .functionalities import load_csv
 
 import logging
 from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, relationship
 from sqlalchemy import String
 from repository.main import get_engine, DATA_PATH
+import os
+import datetime
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -28,34 +31,92 @@ def insert_gebruiker_data(gebruiker_data, session):
     session.commit()
     
 
+'''
+USAGE:
+Create the directories "old" and "new" in the data folder.
+Place new CSV file in the "new" folder.
+Run the seeding.
+The processed CSV file will be moved to the "old" folder with a timestamp.
+The "new" folder will now be empty.
+Place a new CSV file in the "new" folder to add more new data.
+Run the seeding again.
+'''
+
+
+# verplaatst het csv bestand naar de "old" map met een timestamp om naamconflicten te voorkomen en data bij te houden
+def move_csv_file(csv_path, destination_folder):
+    base_name = os.path.basename(csv_path)
+    file_name, file_extension = os.path.splitext(base_name)
+
+    timestamp_str = datetime.datetime.now().strftime("%Y_%m_%d_%H-%M-%S")
+    new_path = os.path.join(destination_folder, f"{file_name}_{timestamp_str}{file_extension}")
+    
+    os.rename(csv_path, new_path)
+    
+
+# geeft een lijst van alle ids die al in de database zitten door de tabel te queryen, enkel de id kolom wordt teruggegeven
+def get_existing_ids(session):
+    return [result[0] for result in session.query(Gebruiker.GebruikerId).all()]
+
+
 def seed_gebruiker():
     engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
     
-    logger.info("Reading CSV...")
-    csv = DATA_PATH + '/Gebruikers.csv'
-    df = pd.read_csv(csv, delimiter=",", encoding='utf-8-sig', keep_default_na=True, na_values=['']) # utf-8-sig is nodig om rare tekens te vermijden die er wel zijn bij utf-8
+    # haal alle ids op die al in de database zitten
+    existing_ids = get_existing_ids(session)
     
-    df = df.replace({np.nan: None})
+    # pad naar de csv bestanden, deze moeten in de mappen "old" en "new" zitten
+    old_csv_dir = os.path.join(DATA_PATH, "old")
+    new_csv_dir = os.path.join(DATA_PATH, "new")
     
+    # check of de mappen "old" en "new" bestaan
+    if not os.path.exists(old_csv_dir) or not os.path.exists(new_csv_dir):
+        raise FileNotFoundError("The folders 'old' and 'new' must exist in the data folder")
+
+    folder_new = new_csv_dir
     gebruiker_data = []
-    logger.info("Seeding inserting rows")
-    progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+    
+        # enkel de csv bestanden in de "new" map worden ingelezen, de bestanden in de "old" map zijn reeds verwerkt
+    for filename in os.listdir(folder_new):
+        if filename == 'Gebruikers.csv':
+            csv_path = os.path.join(folder_new, filename)
+            logger.info(f"Reading CSV: {csv_path}")
+            df, error = load_csv(csv_path)
+            if error:
+                raise Exception(f"Error loading CSV: {csv_path, error}")
+            
+            # verwijder de rijen waarvan de id al in de database zit
+            df = df[~df["crm_Gebruikers_CRM_User_ID"].isin(existing_ids)]
 
-    for _, row in df.iterrows():
-        # print(row)
-        p = Gebruiker(
-            GebruikerId=row["crm_Gebruikers_CRM_User_ID"],
-            BusinessUnitNaam=row["crm_Gebruikers_Business_Unit_Naam_"]
-        )
-        gebruiker_data.append(p)
+            df = df.replace({np.nan: None})
+            
+            logger.info("Seeding inserting rows")
+            progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+            
+            chunks = [df[i:i + BATCH_SIZE] for i in range(0, df.shape[0], BATCH_SIZE)]
+            for chunk in chunks:
+                gebruiker_data = []
+                for _, row in chunk.iterrows(): 
+                    p = Gebruiker(
+                        GebruikerId=row["crm_Gebruikers_CRM_User_ID"],
+                        BusinessUnitNaam=row["crm_Gebruikers_Business_Unit_Naam_"]
+                    )
+                    gebruiker_data.append(p)
+
+                insert_gebruiker_data(gebruiker_data, session)
+                progress_bar.update(len(gebruiker_data))
+
+            progress_bar.close()
+
+            # verplaats het csv bestand naar de "old" map voor reeds verwerkte bestanden
+            move_csv_file(csv_path, old_csv_dir)
+
+            logger.info(f"Number of new (non-duplicate) rows found in {csv_path}: {len(gebruiker_data)}")
+    
+    # als er geen nieuwe data is, dan is de lijst leeg
+    if not gebruiker_data:
+        logger.info("No new data was given. Data is up to date already.")
         
-        if len(gebruiker_data) >= BATCH_SIZE:
-            insert_gebruiker_data(gebruiker_data, session)
-            gebruiker_data = []
-            progress_bar.update(BATCH_SIZE)
-
-    if gebruiker_data:
-        insert_gebruiker_data(gebruiker_data, session)
-        progress_bar.update(len(gebruiker_data))
+    session.close()
