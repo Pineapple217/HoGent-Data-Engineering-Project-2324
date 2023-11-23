406c14ffb3af915dab3f1c422ba8e828af25ad55
WardDeDeckere <ward.dedeckere@student.hogent.be>
2023-11-23 17:27:56 +0100

    Mailing Epic 2 update

--- src/repository/mailing.py
+++ src/repository/mailing.py
@@ -1,11 +1,12 @@
 from .base import Base
-
+from .functionalities import load_csv, move_csv_file
 import logging
 from sqlalchemy.orm import Mapped
 from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy import String, Date
 from sqlalchemy.orm import sessionmaker
 from repository.main import get_engine, DATA_PATH
+import os
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -37,39 +38,61 @@ def insert_mailings_data(pageviews_data, session):
     session.bulk_save_objects(pageviews_data)
     session.commit()
 
+def get_existing_ids(session):
+    return [result[0] for result in session.query(Mailing.MailingId).all()]
 
 def seed_mailing():
     engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
-    logger.info("Reading CSV...")
-    csv = DATA_PATH + "/CDI mailing.csv"
-    df = pd.read_csv(csv, delimiter=",", encoding="utf-8", keep_default_na=True, na_values=[""])
+
+    existing_ids = get_existing_ids(session)
+
+    old_csv_dir = os.path.join(DATA_PATH, "old")
+    new_csv_dir = os.path.join(DATA_PATH, "new")
+    if not os.path.exists(old_csv_dir) or not os.path.exists(new_csv_dir):
+        raise FileNotFoundError("The folders 'old' and 'new' must exist in the data folder")
     
-    # Sommige lege waardes worden als NaN ingelezen
-    # NaN mag niet in een varchar
-    df = df.replace({np.nan: None})
-    
-    df["crm_CDI_Mailing_Sent_On"] = pd.to_datetime(df["crm_CDI_Mailing_Sent_On"], format="%d-%m-%Y")
-    
+    folder_new = new_csv_dir
+
     mailing_data = []
-    logger.info("Seeding inserting rows")
-    progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
-    for _, row in df.iterrows():
-        p = Mailing(
-            MailingId     = row['crm_CDI_Mailing_Mailing'],
-            Name        = row['crm_CDI_Mailing_Name'],
-            SentOn      = row['crm_CDI_Mailing_Sent_On'],
-            Onderwerp   = row['crm_CDI_Mailing_Onderwerp'],
-        )
-        mailing_data.append(p)
+    for filename in os.listdir(folder_new): #check alle filenames in 'new'
+        if filename == 'CDI mailing.csv': #hardcoded filename, zodat startswith niet fout kan lopen
+            csv_path = os.path.join(folder_new, filename) #vul filepath aan met gevonden file
+    
+            logger.info(f"Reading CSV: {csv_path}")
+            df, error = load_csv(csv_path) #load_csv uit functionalities.py, probeert met hardcoded delimiters en encodings een df te maken
+            if error:
+                raise Exception(f"Error loading CSV: {csv_path, error}")
+            
+            df = df.replace({np.nan: None})
+            df["crm_CDI_Mailing_Sent_On"] = pd.to_datetime(df["crm_CDI_Mailing_Sent_On"], format="%d-%m-%Y")
 
-        if len(mailing_data) >= BATCH_SIZE:
-            insert_mailings_data(mailing_data, session)
-            mailing_data = []
-            progress_bar.update(BATCH_SIZE)
+            df = df[~df['crm_CDI_Mailing_Mailing'].isin(existing_ids)]
 
-    # Insert any remaining data
-    if mailing_data:
-        insert_mailings_data(mailing_data, session)
-        progress_bar.update(len(mailing_data))
+            chunks = [df[i:i + BATCH_SIZE] for i in range(0, df.shape[0], BATCH_SIZE)]
+
+            progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+
+            for chunk in chunks:
+                mailing_data = []
+                for _, row in chunk.iterrows():
+                    p = Mailing(
+                        MailingId   = row['crm_CDI_Mailing_Mailing'],
+                        Name        = row['crm_CDI_Mailing_Name'],
+                        SentOn      = row['crm_CDI_Mailing_Sent_On'],
+                        Onderwerp   = row['crm_CDI_Mailing_Onderwerp'],
+                    )
+                    mailing_data.append(p)
+
+                insert_mailings_data(mailing_data, session)
+                progress_bar.update(len(mailing_data))
+
+            progress_bar.close()
+
+            move_csv_file(csv_path, old_csv_dir)
+
+            logger.info(f"Number of new (non-duplicate) rows found in {csv_path}: {len(df)}")
+
+    if not mailing_data:
+        logger.info("No new data was given. Data is up to date already.")
