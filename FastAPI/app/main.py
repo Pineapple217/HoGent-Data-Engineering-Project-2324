31f4c0fddc44ba14919a9f7244ffc65e09d1a718
JulesRosier <julesrosier@student.hogent.be>
2023-11-24 14:52:03 +0100

    Server meerdere ids

--- FastAPI/app/main.py
+++ FastAPI/app/main.py
@@ -5,19 +5,21 @@ from dotenv import load_dotenv
 from sqlalchemy import create_engine
 from sklearn.metrics.pairwise import cosine_similarity
 
-from fastapi import FastAPI
+from fastapi import FastAPI, Query
+from typing import Annotated, List
 
 load_dotenv()
 DB_URL = os.getenv("DB_URL")
 engine = create_engine(DB_URL)
 query = """
-    select c.ContactPersoonId, i.CampagneId, i.CampagneNaam, a.Ondernemingsaard, a.Ondernemingstype, a.PrimaireActiviteit, f.Naam as Functie
+    select c.ContactPersoonId, i.CampagneId, i.CampagneNaam, ca.Startdatum, a.Ondernemingsaard, a.Ondernemingstype, a.PrimaireActiviteit, f.Naam as Functie
     from Contactfiche c
     join Account a on a.AccountId = c.AccountId
     join Inschrijving i on i.ContactficheId = c.ContactPersoonId
+    join Campagne ca on ca.CampagneId = i.CampagneId
     join ContactficheFunctie cf on cf.ContactpersoonId = c.ContactPersoonId
     join Functie f on f.FunctieId = cf.FunctieId
-    where CampagneId is not null;
+    where i.CampagneId is not null;
 """
 df = pd.read_sql(query, engine)
 df.set_index('ContactPersoonId', inplace=True)
@@ -25,19 +27,40 @@ df["rating"] = 1
 df_pivot = pd.pivot_table(df, index='ContactPersoonId', columns=['Ondernemingsaard', 'Ondernemingstype', 'PrimaireActiviteit', 'Functie'], values='rating', fill_value = 0)
 
 
-def calc(contact_id: str):
-    select_contact = contact_id
-    similarity_matrix = cosine_similarity(df_pivot, [df_pivot.loc[select_contact]]).reshape(1,-1)[0]
-    similarities = df_pivot.index.join(similarity_matrix)
-    similarities = pd.DataFrame({'ContactPersoonId':df_pivot.index, 'sim':similarity_matrix}).set_index('ContactPersoonId')
-    similar_users = similarities[similarities['sim'] > 0.75].sort_values(by='sim', ascending=False)
-    done_campaigns = df.loc[df.index == select_contact]['CampagneId']
-    similar_campagnes = df[df.index.isin(similar_users.index)].replace(0, np.nan).dropna(axis=1, how='all')
-    similar_campagnes_not_done = similar_campagnes[~similar_campagnes['CampagneId'].isin(done_campaigns)][['CampagneId', 'CampagneNaam']]
-    similar_campagnes_not_done.set_index('CampagneId', inplace=True)
-    similar_campagnes_not_done.drop_duplicates(inplace=True)
-    return similar_campagnes_not_done
-
+def calc(contact_ids: list):
+    # select_contact = ['DA252429-E5A6-ED11-AAD1-6045BD8956C9', '915D6FF4-A972-E111-B43A-00505680000A']
+    select_contact = contact_ids
+    try:
+        ss = df_pivot.loc[select_contact]
+    except:
+        return "id does not exits"
+    try: 
+        similarity_matrix = cosine_similarity(df_pivot, ss)
+    except:
+        return "geen sims"
+    similarity_matrix_df = pd.DataFrame(similarity_matrix, index=df_pivot.index, columns=ss.index)
+    stacked_similarity_df = similarity_matrix_df.stack()
+    high_similarity_df = pd.DataFrame(stacked_similarity_df[stacked_similarity_df > 0.5])
+    high_similarity_df.index.names = ['Similars', 'Selected']
+    high_similarity_df.reset_index(inplace=True)
+    high_similarity_df.set_index(['Selected', 'Similars'], inplace=True)
+    high_similarity_df.sort_index(level='Selected', inplace=True)
+    done_campaigns = df.loc[select_contact, ["CampagneId"]]
+    results = []
+    for selected, s_df in high_similarity_df.groupby(level=0):
+        t = df[df.index.isin(s_df.reset_index()['Similars'])].replace(0, np.nan).dropna(axis=1, how='all')
+        t.sort_values('Startdatum', ascending=False, inplace=True)
+        t = t[t['Startdatum'] > '2023-11-06']
+        results.append((selected, t))
+    r = []
+    for s, x in results:
+        similar_campagnes_not_done = x[~x['CampagneId'].isin(done_campaigns.loc[s])][['CampagneId', 'CampagneNaam']]
+        similar_campagnes_not_done.drop_duplicates(inplace=True)
+        similar_campagnes_not_done.set_index('CampagneId')
+        x = similar_campagnes_not_done.reset_index()[['CampagneId', 'CampagneNaam']].set_index('CampagneId').to_dict()
+        x['cantact'] = s
+        r.append(x)
+    return r
 
 app = FastAPI()
 
@@ -46,6 +69,7 @@ app = FastAPI()
 def read_root():
     return {"health": "OK"}
 
-@app.get("/contact/{contact_id}")
-def contact_c(contact_id: str):
-    return calc(contact_id)
+@app.get("/contact")
+def contact_c(ids: str = Query(None)):
+    ids = ids.split(",")
+    return calc(ids)
