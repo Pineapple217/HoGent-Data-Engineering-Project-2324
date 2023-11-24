40dc045d688fabbe084a51e5ca2b0f877620d00f
Hannelore Mergan <hannelore.mergan@student.hogent.be>
2023-11-24 13:52:26 +0100

    Epic 2 sessie inschrijving

--- src/repository/sessie_inschrijving.py
+++ src/repository/sessie_inschrijving.py
@@ -1,4 +1,5 @@
 from .base import Base
+from .functionalities import load_csv, move_csv_file
 
 import logging
 from sqlalchemy.orm import Mapped
@@ -6,6 +7,7 @@ from sqlalchemy.orm import mapped_column, relationship
 from sqlalchemy import String, ForeignKey, text
 from sqlalchemy.orm import sessionmaker
 from repository.main import get_engine, DATA_PATH
+import os
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -40,64 +42,102 @@ def insert_sessie_inschrijving_data(sessie_inschrijving_data, session):
     session.commit()
 
 
+'''
+USAGE:
+Create the directories "old" and "new" in the data folder.
+Place new CSV file in the "new" folder.
+Run the seeding.
+The processed CSV file will be moved to the "old" folder with a timestamp.
+The "new" folder will now be empty.
+Place a new CSV file in the "new" folder to add more new data.
+Run the seeding again.
+'''
+
+    
+# geeft een lijst van alle ids die al in de database zitten door de tabel te queryen, enkel de id kolom wordt teruggegeven
+def get_existing_ids(session):
+    return [result[0] for result in session.query(SessieInschrijving.SessieInschrijvingId).all()]
+
+
 def seed_sessie_inschrijving():
     engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
-    logger.info("Reading CSV...")
-    csv = DATA_PATH + "/Sessie inschrijving.csv"
-    df = pd.read_csv(csv, delimiter=",", encoding="utf-8", keep_default_na=True, na_values=[""],)
     
-    # csv bevat veel lege rijen, dit drop alle rijen die volledig leeg zijn
-    df = df.dropna(how="all", axis=0)
+    # haal alle ids op die al in de database zitten
+    existing_ids = get_existing_ids(session)
     
-    # Sommige lege waardes worden als NaN ingelezen
-    # NaN mag niet in een varchar
-    df = df.replace({np.nan: None})
+    # pad naar de csv bestanden, deze moeten in de mappen "old" en "new" zitten
+    old_csv_dir = os.path.join(DATA_PATH, "old")
+    new_csv_dir = os.path.join(DATA_PATH, "new")
     
+    # check of de mappen "old" en "new" bestaan
+    if not os.path.exists(old_csv_dir) or not os.path.exists(new_csv_dir):
+        raise FileNotFoundError("The folders 'old' and 'new' must exist in the data folder")
+
+    folder_new = new_csv_dir
     sessie_inschrijving_data = []
-    logger.info("Seeding inserting rows")
-    progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
-    for _, row in df.iterrows():
-        p = SessieInschrijving(
-            SessieInschrijvingId=row["crm_SessieInschrijving_SessieInschrijving"],
-            SessieId=row["crm_SessieInschrijving_Sessie"],
-            InschrijvingId=row["crm_SessieInschrijving_Inschrijving"],
-        )
-        sessie_inschrijving_data.append(p)
+    
+    for filename in os.listdir(folder_new):
+        if filename == 'Sessie inschrijving.csv':
+            csv_path = os.path.join(folder_new, filename)
+            logger.info(f"Reading CSV: {csv_path}")
+            df, error = load_csv(csv_path)
+            if error:
+                raise Exception(f"Error loading CSV: {csv_path, error}")
+            
+            # verwijder de rijen waarvan de id al in de database zit
+            df = df[~df["crm_SessieInschrijving_SessieInschrijving"].isin(existing_ids)]
+            
+            df = df.dropna(how="all", axis=0)
+    
+            df = df.replace({np.nan: None})
+    
+            logger.info("Seeding inserting rows")
+            progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+            
+            # data in chunks steken
+            chunks = [df[i:i + BATCH_SIZE] for i in range(0, df.shape[0], BATCH_SIZE)]
+            for chunk in chunks:
+                sessie_inschrijving_data = []
+                for _, row in chunk.iterrows(): 
+                    p = SessieInschrijving(
+                        SessieInschrijvingId=row["crm_SessieInschrijving_SessieInschrijving"],
+                        SessieId=row["crm_SessieInschrijving_Sessie"],
+                        InschrijvingId=row["crm_SessieInschrijving_Inschrijving"],
+                    )
+                    sessie_inschrijving_data.append(p)
 
-        if len(sessie_inschrijving_data) >= BATCH_SIZE:
-            insert_sessie_inschrijving_data(sessie_inschrijving_data, session)
-            sessie_inschrijving_data = []
-            progress_bar.update(BATCH_SIZE)
+                insert_sessie_inschrijving_data(sessie_inschrijving_data, session)
+                progress_bar.update(len(sessie_inschrijving_data))
 
-    # Insert any remaining data
-    if sessie_inschrijving_data:
-        insert_sessie_inschrijving_data(sessie_inschrijving_data, session)
-        progress_bar.update(len(sessie_inschrijving_data))
+            progress_bar.close()
 
-    session.execute(
-        text(
-            """
+            # verplaats het csv bestand naar de "old" map voor reeds verwerkte bestanden
+            move_csv_file(csv_path, old_csv_dir)
+
+            logger.info(f"Number of new (non-duplicate) rows found in {csv_path}: {len(df)}")
+    
+    # als er geen nieuwe data is, dan is de lijst leeg
+    if not sessie_inschrijving_data:
+        logger.info("No new data was given. Data is up to date already.")
+    
+    session.execute(text("""
         UPDATE SessieInschrijving
         SET SessieInschrijving.SessieId = NULL
         WHERE SessieInschrijving.SessieId
         NOT IN
         (SELECT SessieId FROM Sessie)
-    """
-        )
-    )
+    """))
     session.commit()
-    
-    session.execute(
-        text(
-            """
+
+    session.execute(text("""
         UPDATE SessieInschrijving
         SET SessieInschrijving.InschrijvingId = NULL
         WHERE SessieInschrijving.InschrijvingId
         NOT IN
         (SELECT InschrijvingId FROM Inschrijving)
-    """
-        )
-    )
+    """))
     session.commit()
+        
+    session.close()
