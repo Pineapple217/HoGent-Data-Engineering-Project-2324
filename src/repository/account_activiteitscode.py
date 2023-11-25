b59c2614c92d88824eb92f6a499f027162e2d670
Hannelore Mergan <hannelore.mergan@student.hogent.be>
2023-11-25 13:17:35 +0100

    Epic 2 account activiteitscode

--- src/repository/account_activiteitscode.py
+++ src/repository/account_activiteitscode.py
@@ -1,9 +1,11 @@
 from .base import Base
+from .functionalities import load_csv, move_csv_file
 
 import logging
 from sqlalchemy.orm import Mapped, mapped_column, sessionmaker, relationship
 from sqlalchemy import String, ForeignKey, text
 from repository.main import get_engine, DATA_PATH
+import os
 import pandas as pd
 import numpy as np
 from tqdm import tqdm
@@ -31,44 +33,91 @@ class AccountActiviteitscode(Base):
     Activiteit: Mapped["Activiteitscode"] = relationship(back_populates="AccountActiviteitscode")
 
 
-def insert_accountActiviteitscode_data(accountActiviteitscode_data, session):
-    session.bulk_save_objects(accountActiviteitscode_data)
+def insert_account_activiteitscode_data(account_activiteitscode_data, session):
+    session.bulk_save_objects(account_activiteitscode_data)
     session.commit()
 
+
+'''
+USAGE:
+Create the directories "old" and "new" in the data folder.
+Place new CSV file in the "new" folder.
+Run the seeding.
+The processed CSV file will be moved to the "old" folder with a timestamp.
+The "new" folder will now be empty.
+Place a new CSV file in the "new" folder to add more new data.
+Run the seeding again.
+'''
+
+
+# geeft een lijst van alle campagne ids die al in de database zitten door de campagne tabel te queryen, enkel de campagne id kolom wordt teruggegeven
+def get_existing_ids(session):
+    return [result[0] for result in session.query(AccountActiviteitscode.AccountActiviteitscodeId).all()]
 
 def seed_account_activiteitscode():
     engine = get_engine()
     Session = sessionmaker(bind=engine)
     session = Session()
     
-    logger.info("Reading CSV...")
-    csv = DATA_PATH + "/Account activiteitscode.csv"
-    df = pd.read_csv(csv, delimiter=",", encoding="utf-8", keep_default_na=True, na_values=[""])
+    # haal alle campagne ids op die al in de database zitten
+    existing_ids = get_existing_ids(session)
     
-    df = df.dropna(how="all", axis=0)
-    df = df.replace({np.nan: None})
-
-    accountActiviteitscode_data = []
-    logger.info("Seeding inserting rows")
-    progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+    # pad naar de csv bestanden, deze moeten in de mappen "old" en "new" zitten
+    old_csv_dir = os.path.join(DATA_PATH, "old")
+    new_csv_dir = os.path.join(DATA_PATH, "new")
     
-    for _, row in df.iterrows():
-        p = AccountActiviteitscode(
-            AccountId=row["crm_Account_ActiviteitsCode_Account"],
-            ActiviteitsId=row["crm_Account_ActiviteitsCode_Activiteitscode"],
-            AccountActiviteitscodeId=row["crm_Account_ActiviteitsCode_inf_account_inf_activiteitscodeId"],
-        )
-        accountActiviteitscode_data.append(p)
+    # check of de mappen "old" en "new" bestaan
+    if not os.path.exists(old_csv_dir) or not os.path.exists(new_csv_dir):
+        raise FileNotFoundError("The folders 'old' and 'new' must exist in the data folder")
 
-        if len(accountActiviteitscode_data) >= BATCH_SIZE:
-            insert_accountActiviteitscode_data(accountActiviteitscode_data, session)
-            accountActiviteitscode_data = []
-            progress_bar.update(BATCH_SIZE)
+    folder_new = new_csv_dir
+    account_activiteitscode_data = []
+    
+    # enkel de csv bestanden in de "new" map worden ingelezen, de bestanden in de "old" map zijn reeds verwerkt
+    for filename in os.listdir(folder_new):
+        if filename == 'Account activiteitscode.csv':
+            csv_path = os.path.join(folder_new, filename)
+            logger.info(f"Reading CSV: {csv_path}")
+            df, error = load_csv(csv_path)
+            if error:
+                raise Exception(f"Error loading CSV: {csv_path, error}")
+            
+            # verwijder de rijen waarvan de campagne id al in de database zit
+            df = df[~df["crm_Account_ActiviteitsCode_inf_account_inf_activiteitscodeId"].isin(existing_ids)]
+            
+            # data cleaning
+            df = df.dropna(how="all", axis=0)
+            df = df.replace({np.nan: None})
+            
+            logger.info("Seeding inserting rows")
+            progress_bar = tqdm(total=len(df), unit=" rows", unit_scale=True)
+            
+            # data in chunks steken
+            chunks = [df[i:i + BATCH_SIZE] for i in range(0, df.shape[0], BATCH_SIZE)]
+            for chunk in chunks:
+                account_activiteitscode_data = []
+                for _, row in chunk.iterrows(): 
+                    p = AccountActiviteitscode(
+                        AccountId=row["crm_Account_ActiviteitsCode_Account"],
+                        ActiviteitsId=row["crm_Account_ActiviteitsCode_Activiteitscode"],
+                        AccountActiviteitscodeId=row["crm_Account_ActiviteitsCode_inf_account_inf_activiteitscodeId"],
+                    )
+                    account_activiteitscode_data.append(p)
 
-    if accountActiviteitscode_data:
-        insert_accountActiviteitscode_data(accountActiviteitscode_data, session)
-        progress_bar.update(len(accountActiviteitscode_data))
+                insert_account_activiteitscode_data(account_activiteitscode_data, session)
+                progress_bar.update(len(account_activiteitscode_data))
 
+            progress_bar.close()
+
+            # verplaats het csv bestand naar de "old" map voor reeds verwerkte bestanden
+            move_csv_file(csv_path, old_csv_dir)
+
+            logger.info(f"Number of new (non-duplicate) rows found in {csv_path}: {len(df)}")
+    
+    # als er geen nieuwe data is, dan is de lijst leeg
+    if not account_activiteitscode_data:
+        logger.info("No new data was given. Data is up to date already.")
+    
     session.execute(text("""
         DELETE FROM AccountActiviteitscode
         WHERE AccountId NOT IN 
@@ -82,3 +131,5 @@ def seed_account_activiteitscode():
             (SELECT ActiviteitsId FROM Activiteitscode);
     """)) #delete, want niet bruikbaar met null
     session.commit()
+      
+    session.close()
